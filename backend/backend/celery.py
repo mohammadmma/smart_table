0c75365d67c06004dbdb352d8279107c12a23915
mohammadmma <mohammadmma3004@gmail.com>
2025-08-22 19:00:55 +0330

    create celery application (app for short)

---
+++ backend/backend/celery.py
@@ -0,0 +1,16 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
+app = Celery(
+    'backend',
+    broker='redis://localhost:6379/0',
+)
+
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
